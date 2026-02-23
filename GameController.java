1f50a22cb3f5bf3300e51c5df8299d189ee23634
amitkr069 <aman3133@7-11.com>
2026-02-23 16:57:28 +0530

    Added the Restart app and Exit app feature

---
+++ GameController.java
@@ -0,0 +1,12 @@
+/**
+@author amit
+@version 6
+this clas is for use case 6. it is used to restartGame and exit game
+*/
+import java.util.Scanner;
+class GameController{
+	public static boolean restartGame(Scanner sc){
+		System.out.print("Do you want to play agian? (yes/no): ");
+		return sc.nextLine().equalsIgnoreCase("yes");
+	}
+}

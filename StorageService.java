5b374b8fcc8ef82f08d2452f7c41229702def8eb
amitkr069 <aman3133@7-11.com>
2026-02-23 16:40:49 +0530

    Implemented Storage Service

---
+++ StorageService.java
@@ -0,0 +1,22 @@
+/**
+@Author Amit
+@versoin 5
+
+This class is part of use case 5
+We are using JDBC
+*/
+import java.io.*;
+public class StorageService{
+	public static void saveResult(String player, int attempts, boolean win){
+		//System.out.println("Hii")
+		try(BufferedWriter writer = new BufferedWriter(
+			new FileWriter("game_results.txt", true)
+		)){
+			writer.write("Player: " + player + ", Attempts: " + attempts + ", Result: " + (win ? "WIN" : "LOSE"));
+			writer.newLine();
+		}
+		catch(IOException e){
+			System.out.println("Unable to save game result.");
+		}
+	}
+}

c52731e41ef9561a1dfde2d10941bafd7280e350
Amit Kumar Mandal <126327335+amitkr069@users.noreply.github.com>
2026-02-23 21:42:43 +0530

    Initialized Guessing App

--- Guessing.java
+++ Guessing.java
@@ -1,10 +1,18 @@
+/**
+@author amit
+@version 1.0
+
+This is Use Case1.
+In this just the basic implementation of Guessing App has been implemented.
+*/
+
 import java.util.Random;
 import java.util.Scanner;
 public class Guessing {
 	public static void main(String[] args){
 		System.out.println("Welcome to guessing app");
-		GameConfig game = new GameConfig();
-		game.showRules();
+		GameConfig game = new GameConfig(); // Creating object of GameConfig
+		game.showRules(); // calling the function of GameConfig class to show the Rules.
 		
 		
 	}
